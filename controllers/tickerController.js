e6b80b3f9b179c1cadd95b3798fe620a1aa56646
Pedro Piloto <pedro@pedropiloto.com>
2021-04-26 00:00:55 +0100

    bigger numbers

--- controllers/tickerController.js
+++ controllers/tickerController.js
@@ -3,7 +3,7 @@ const tickers = require("./tickers");
 
 
   const get = (req, res, next) => {
-    res.send(`${Math.floor(Math.random() * 1000)}`)
+    res.send(`${Math.floor(Math.random() * 1000000)}`)
   }
 
   const getTickers =  (req, res, next)  => {
