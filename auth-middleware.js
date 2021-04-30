d495d8ce0b31922d04203c4f6fca5a7b502b3137
Pedro Piloto <pedro@pedropiloto.com>
2021-04-30 01:30:58 +0100

    logs

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,5 +1,7 @@
 module.exports = (req, res, next) => {
   const api_key = req.headers['api_key']
+  console.log('api_key', api_key)
+  console.log('process.env.API_KEY', process.env.API_KEY)
   if (!!process.env.API_KEY && api_key !== process.env.API_KEY) {
     res.status(401).json({
       error: 'Unauthorized'
