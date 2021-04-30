e2f5adfbd25135130a4035454b4525446ac87ea0
Pedro Piloto <pedro@pedropiloto.com>
2021-04-30 01:29:59 +0100

    update

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,20 +1,10 @@
-const newrelic = require('newrelic');
-
 module.exports = (req, res, next) => {
-  try {
-    const device_mac_address = req.headers['device_mac_address']
-    console.log("headers", req.headers)
-    const api_key = req.headers['api_key']
-    if (!!process.env.API_KEY && api_key !== process.env.API_KEY) {
-      res.status(401).json({
-        error: new Error('Unauthorized')
-      });
-    } else {
-      next();
-    }
-  } catch {
+  const api_key = req.headers['api_key']
+  if (!!process.env.API_KEY && api_key !== process.env.API_KEY) {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: 'Unauthorized'
     });
+  } else {
+    next();
   }
 };
